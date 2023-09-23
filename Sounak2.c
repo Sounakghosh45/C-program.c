59fe6dd279473a278466f33795a61bd4da2517fc
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-09-23 11:59:54 +0530

    Create Sounak2.c
    Program of C

---
+++ Sounak2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+  float a,b,c,s,ar;
+  printf("Enter the value of three side A,B and C:\n");
+  scanf("%f %f %f",&a,&b,&c);
+  s=(a+b+c)*0.5;
+  ar=sqrt(s*(s-a)*(s-b)*(s-c));
+  printf("The area of the triangle is=%f",ar);
+
+    return 0;
+}
