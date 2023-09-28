cbbff650ab53941ebea6c0d09ac4a0dec5938ccd
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-09-28 12:50:12 +0530

    Create right angle.c

---
+++ right angle.c
@@ -0,0 +1,14 @@
+
+include <stdio.h>
+#include<math.h>
+int main()
+{
+  float a,b,c,x,y;
+  printf("Enter the length of three side A,B and C:\n");
+  scanf("%f %f %f",&a,&b,&c);
+x=pow(a,2)+pow(b,2);
+y=pow(c,2);
+int sl=(x==y)?(x==y):(x==y);
+printf("%d",sl);
+  
+}
