c04a111c7078a4a56e20a58a379d6f514704e8d7
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-09-28 12:34:05 +0530

    Create Biodata.c

---
+++ Biodata.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+  char n,p,e;
+int b;
+printf("Enter your name");
+scanf("%s",&n);
+printf("\n Enter Programme:");
+scanf("%s",&p);
+printf("\n Enter your mail id:");
+ scanf("%s",&e);
+printf("\n Enter your date of birth:");
+  scanf("%s",&b);
+printf("calculate age=%d",2023-b);
+
