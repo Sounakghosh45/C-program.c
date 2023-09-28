d097a0278c24e77e342532dac06970ec073c39ba
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-09-28 12:40:59 +0530

    Create Size of datatypes.c

---
+++ Size of datatypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("Size of char: %lu bytes\n", sizeof(char));
+    printf("Size of short: %lu bytes\n", sizeof(short));
+    printf("Size of int: %lu bytes\n", sizeof(int));
+    printf("Size of long: %lu bytes\n", sizeof(long));
+    printf("Size of long long: %lu bytes\n", sizeof(long long));
+    printf("Size of float: %lu bytes\n", sizeof(float));
+    printf("Size of double: %lu bytes\n", sizeof(double));
+    printf("Size of long double: %lu bytes\n", sizeof(long double));
+
+  
+}
