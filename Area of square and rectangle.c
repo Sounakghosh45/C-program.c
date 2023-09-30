03e98aa7f6953a8cf42dc570fc8162687acdb702
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-01 00:43:59 +0530

    Create Area of square and rectangle.c

---
+++ Area of square and rectangle.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main (){
+  int a,b;
+  printf("Enter the value of A and B:\n");
+  scanf("%d %d",&a,&b);
+if(a==b){
+printf("This is a square\n");
+printf(" Area of square is:%d",a*b);
+}
+
+{
+printf("This is a rectangle\n");
+printf("Area of rectangle is:%d",a*b);
+}
+}
