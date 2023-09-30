92828975407171cd4c49b02280de9f5e70317900
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-01 00:58:28 +0530

    Boiling temperature of water.c

---
+++ Boiling temperature of water.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+int main (){
+  int temp;
+  printf("Enter the value of Temperature in celcius:\n");
+  scanf("%d",&temp);
+if(temp==100){
+printf("This is Boiling Point of water\n");
+}
+else
+{
+printf("This is not Boiling Point of water\n");
+}
+}
