d5671e3361a9c8a182e0df67b686c60aedadc7a6
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-02 11:02:31 +0530

    Create Currency converter.c

---
+++ Currency converter.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main (){
+ float rupees;
+printf("Enter the Amount in Rupees:\n");
+  scanf("%f",&rupees);
+printf("Amount in Dollar:%f\n",rupees*0.012);
+printf("Amount in Rubel:%f\n",rupees*1.16);
+printf("Amount in Yen:%f\n",rupees*1.17);
+printf("Amount in Euros:%f\n",rupees*0.011);
+}
