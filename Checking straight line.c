a80b19c393cf976422a21a2564239cafc99f2852
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-02 11:38:17 +0530

    Create Checking straight line.c

---
+++ Checking straight line.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+ #include<math.h> 
+ int main(){ 
+     float y,m,x,c; 
+     printf("enter the value of  y,m,x and c \n"); 
+     scanf("%f %f %f %f",&y,&m,&x,&c); 
+     float mxc=m*x+c; 
+     int sl=(y==mxc)?(y==mxc):(y==mxc); 
+     printf("%d",sl); 
+ }
