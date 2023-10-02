5b894e864323f1d34ba907404f4d6a08976037a2
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-02 11:08:23 +0530

    Create Area of hexagon.c

---
+++ Area of hexagon.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+ float area,a;
+ printf("Enter the value of side of hexagon:\n");
+ scanf("%f",&a);
+ area=3*1.732*0.5*a*a;
+ printf("Area of the hexagon is=%f",area);
+}
