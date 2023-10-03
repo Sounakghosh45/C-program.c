c1d0c500f4d6187b92d2ec6b0151aeef9a768eb7
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-03 22:53:56 +0530

    Create Beep sound.c

---
+++ Beep sound.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+ int a,b,s_1;
+ printf("Enter the value of A:");
+ scanf("%d",&a);
+ printf("Enter the value of B:");
+ scanf("%d",&b);
+ s_1=a+b;
+ printf("The sum of two variables is:%d\a",s_1);
+}
