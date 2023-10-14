ed2f7cb23a4463ad367e316b701b5bc2e74b6a46
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-10-14 23:42:05 +0530

    Create N-th number Hello World.c

---
+++ N-th number Hello World.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main (){
+    int input;
+    printf("Enter the input:\n");
+    scanf("%d",&input);
+    if(input==-1){
+        printf("Hey! it's over");
+    
+    }
+        else
+        {
+            l1:
+            printf("Hello World \n");
+            goto l1;
+            
+        
+        }
+    }
