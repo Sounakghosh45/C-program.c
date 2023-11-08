f56dd2a7c464158349737d90d9c7f1fb2ac2830d
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-08 08:11:57 +0530

    Create Checking even or odd.c

---
+++ Checking even or odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a ,b;
+    printf("enter the value  of a:\n");
+    scanf("%d",&a);
+    b=a/2;
+    if(b==0)
+    printf("a is even");
+    else
+    printf("a is odd");
+}
