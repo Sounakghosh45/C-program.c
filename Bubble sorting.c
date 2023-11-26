1abcf5fab9e0dd5df99607c64069934d3bab231e
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-27 01:38:06 +0530

    Create Bubble sorting.c

---
+++ Bubble sorting.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int arr[]={20,2,30,50,40,60,80,80,9};
+    int start=0;
+    int stop=9;
+    for(int i=0;i<stop;i++)
+    {
+       for(int j=0;j<stop-i-1;j++)
+       {
+           if(arr[j]>arr[j+1])
+           {
+               int temp=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=temp;
+           }
+       }
+    }
+    for(int i=0;i<stop;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+}
