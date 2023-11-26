0b5a3abb5aa5f9aad215fd11d6b03a16bbc7092f
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-27 01:40:40 +0530

    Create Checking existing file.c

---
+++ Checking existing file.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    FILE *fp;
+    fp= fopen("sample.txt","r");
+    if(fp!=NULL)
+    {
+        printf("FILE OPENED!");
+    }
+    else
+    {
+        printf("FILE NOT FOUND!");
+    }
+}
