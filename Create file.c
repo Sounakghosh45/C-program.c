208f04eb4fd1974c5f67624826bce93d68f9558c
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-27 13:40:00 +0530

    Create Create file.c

---
+++ Create file.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    char fname[100];
+    printf("Enter file name to be created >> ");
+    gets(fname);
+
+    FILE *fp;
+    fp=fopen(fname, "w");
+    printf("File successfully created !!! ") 
+    fclose(fp);
+}
