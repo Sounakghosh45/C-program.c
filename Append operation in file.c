63d8c7e7892d7c4dd35a746574de41930b6248f9
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-27 13:33:24 +0530

    Create Append operation in file.c

---
+++ Append operation in file.c
@@ -0,0 +1,21 @@
+int main()
+{
+    char file_name[100],user_data[100];
+    printf("Enter file name:\n");
+    gets(file_name);
+    FILE *fp;
+    fp=fopen(file_name,"a");
+    if(fp!=NULL)
+    {
+        printf("FILE FOUND!");
+        printf("Enter something to be saved in file:\n");
+        gets(user_data);
+        fprintf(fp,"%s",user_data);
+        printf("Data saved in file");
+        fclose(fp);
+    }
+    else
+    {
+        printf("FILE NOT FOUND!");
+    }
+}
