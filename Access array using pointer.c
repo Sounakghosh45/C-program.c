546080e827e0e836fb8e20a90415d299c0d804f9
Sounak Ghosh <145130712+Sounakghosh45@users.noreply.github.com>
2023-11-27 13:41:19 +0530

    Create Access array using pointer.c

---
+++ Access array using pointer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int arry[5];
+    int i;
+    printf("Enter 5 elements for the array: ");
+    for (i = 0; i < 5; ++i)
+        scanf("%d", arry + i);
+    int *ptr = arry;
+    printf("You have entered \n");
+    for(i = 0; i < 5; i++)
+        printf("array[%d] = %d\n",i,*(ptr+i));
+}
